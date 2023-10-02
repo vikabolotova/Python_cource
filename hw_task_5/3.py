61b173cbca1139671659211fdd8080b8eb9c8f31
Bolotova Viktoria <vika2805@mail.ru>
2023-10-02 21:54:04 +0300

    ready

--- hw_task_5/3.py
+++ hw_task_5/3.py
@@ -2,4 +2,12 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(np.random.random((10,10)), index = ['a','b','c','d','e','f','g','h','i','j'])
+df.columns = ['A','B','C','D','E','F','G','H','I','J']
+
+
 print(df)
+print(df.shape)
+print(df.columns)
+print(df.mean().mean())
+df.to_csv('my_random_dataset.csv')
+df.to_csv('my_random_dataset_1.csv', header=False, index = False)
