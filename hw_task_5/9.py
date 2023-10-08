1f21d1d543b3b0828dfce66406c84743eaf26d8c
Bolotova Viktoria <vika2805@mail.ru>
2023-10-09 00:32:30 +0300

    ready

--- hw_task_5/9.py
+++ hw_task_5/9.py
@@ -3,15 +3,19 @@ import pandas as pd
 df = pd.read_csv('BCT-USD.csv')
 df['Date'] = pd.to_datetime(df['Date'])
 
-filtered_df = df[df['Close'] > df['Open']]
+df['Year_month'] = df['Date'].dt.strftime('%Y-%m')
+df_last_day_close = df.groupby('Year_month')['Close'].last().reset_index()
+df_first_day_open = df.groupby('Year_month')['Open'].first().reset_index()
 
-df_months = filtered_df['Date'].dt.month_name().unique()
+df3_merged = pd.merge(df_last_day_close, df_first_day_open)
 
+filtered_df = df3_merged[df3_merged['Close'] > df3_merged['Open']]
+print(filtered_df)
+filtered_df['Year_month'] = pd.to_datetime(filtered_df['Year_month'])
 
-if len(df_months) > 0:
-    print("Месяцы, в которых цена при закрытии была выше цены открытия:")
-    for month in df_months:
-        print(month)
+if filtered_df.empty:
+    print("Нет совпадений")
 else:
-    print("Нет месяцев, в которых цена при закрытии была выше цены открытия.")
+    print(filtered_df['Year_month'].dt.month_name(),filtered_df['Year_month'].dt.year)
+
 
