282219a565e162a8bc276ad13ad28750bcae4d3d
Bolotova Viktoria <vika2805@mail.ru>
2023-10-11 02:20:26 +0300

    ready

--- hw_task_5/8.py
+++ hw_task_5/8.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 df = pd.read_csv('BCT-USD.csv')
 df['Date'] = pd.to_datetime(df['Date'])
-min_volum = df['Volume'].min()
-print(df['Date'][df['Volume'] == min_volum].dt.month_name().iloc[0])
+df['Year_month'] = df['Date'].dt.strftime('%Y-%m')
+df['Year_month'] = pd.to_datetime(df['Year_month'])
 
+df_sum_month = df.groupby('Year_month')['Volume'].sum().reset_index()
+print(df_sum_month)
+
+min_value = df_sum_month.iloc[:, 1].min() # Предполагается, что второй столбец имеет индекс 1
+min_date = df_sum_month[df_sum_month.iloc[:, 1] == min_value]['Year_month'].iloc[0]
+min_month = min_date.month_name()
+min_year = min_date.year
+print(min_month,min_year)
