375ecc1db072c5fbda330a28d6e64695d2f15aa3
Bolotova Viktoria <vika2805@mail.ru>
2023-10-12 23:29:30 +0300

    ready

--- hw_task_5/7.py
+++ hw_task_5/7.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def data_range(start,end):
-    df = pd.read_csv('BCT-USD.csv').set_index('Date')
-    print(df)
-    plt.plot(df.loc[start:end,'Open'], df.loc[start:end,'Close'])
+    df = pd.read_csv('BCT-USD.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df1 = df.set_index('Date')
+    print(df1)
+    plt.plot(df1.loc[start:end,'Open'], df1.loc[start:end,'Close'])
     return plt.savefig('open_close.png')
 
-data_range('2021-10-21','2021-10-29')
 
+data_range('2021-Feb','2023-09')
