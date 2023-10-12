ae6cd3cae3847a2dfc4600966cb7c1bed9f5e8da
Bolotova Viktoria <vika2805@mail.ru>
2023-10-12 23:30:09 +0300

    ready

--- hw_task_5/8.py
+++ hw_task_5/8.py
@@ -8,7 +8,7 @@ df['Year_month'] = pd.to_datetime(df['Year_month'])
 df_sum_month = df.groupby('Year_month')['Volume'].sum().reset_index()
 print(df_sum_month)
 
-min_value = df_sum_month.iloc[:, 1].min() # Предполагается, что второй столбец имеет индекс 1
+min_value = df_sum_month.iloc[:, 1].min()
 min_date = df_sum_month[df_sum_month.iloc[:, 1] == min_value]['Year_month'].iloc[0]
 min_month = min_date.month_name()
 min_year = min_date.year
